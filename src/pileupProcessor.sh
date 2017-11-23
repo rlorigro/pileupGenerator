0fc921595df9aad87200ce5e5497ab61e4a28449
Kishwar <kishwar.shafin@gmail.com>
2017-11-23 13:06:57 -0800

    debug mode off

--- src/pileupProcessor.sh
+++ src/pileupProcessor.sh
@@ -17,12 +17,12 @@ output_dir=$output_dir'run-'$now
 mkdir $output_dir
 echo $output_dir
 mkdir tmp
-# chrs=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT)
-chrs=(3)
+chrs=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT)
+# chrs=(3)
 for i in ${chrs[@]};
     do
     current_output_dir=$output_dir/'chr'$i/
     mkdir $current_output_dir
     echo "Starting chr" $i
-    python3 pileupGenerator.py --bam $bam_file --ref $ref_file --vcf $vcf_file --vcf_region $i --coverage $coverage --window_size $window_size --output_dir $current_output_dir --max_threads $max_threads 2>tmp/progress-$i.txt &
+    python3 pileupGenerator.py --bam $bam_file --ref $ref_file --vcf $vcf_file --vcf_region $i --coverage $coverage --window_size $window_size --output_dir $current_output_dir --max_threads $max_threads 2>tmp/progress-$i.txt
     done
