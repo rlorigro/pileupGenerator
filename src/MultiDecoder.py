9553c52e75663317d42e0bc3137805a0fd52b642
Ryan Lorig-Roach <rlorigro@ucsc.edu>
2017-11-30 00:46:51 -0800

    npy to text or RGBA, see bottom comment for usage

---
+++ src/MultiDecoder.py
@@ -0,0 +1,184 @@
+import numpy
+from PIL import Image
+import os
+
+class Decoder:
+    def __init__(self,summaryFilename,localPath=None):
+        self.summaryFile = summaryFilename
+        self.localPath = localPath
+
+        self.decodeToSNPMap = ['M', 'A', 'C', 'G', 'T', 'I', 'D', 'N', '_']
+        self.decodeToTextMap = ['.', 'A', 'C', 'G', 'T', '-', 'D', 'N', '_']
+        self.decodeIndex = list(range(len(self.decodeToTextMap)))
+
+        self.RGBtoText = [[[' ', 'T'], ['G', 'D']], [['A', '_'], ['C', '.']]]
+
+        self.sortingKey = {'M':5,
+                           'A':0,
+                           'C':1,
+                           'G':2,
+                           'T':3,
+                           'I':6,
+                           'D':4,
+                           'N':7}
+
+        self.noneChar = '_'
+
+        self.SNPtoRGB = {'M': [255,255,255],
+                         'A': [255,0,  0],
+                         'C': [255,255,0],
+                         'G': [0,  255,0],
+                         'T': [0,  0,  255],
+                         'I': [0,  0,  0],
+                         'D': [0,  255,255],
+                         'N': [0,  0,  0],  # redundant in case of read containing 'N'... should this be independent?
+               self.noneChar: [0,  0,  0]}
+
+
+    def decodeArraysToStdout(self):
+        with open(self.summaryFile,'r') as file:
+            for line in file:
+                arrayPath, label = line.split(',')
+
+                if self.localPath is not None:
+                    arrayFilename = arrayPath.split('/')[-1]
+                    arrayPath = self.localPath + arrayFilename
+
+                if os.path.exists(arrayPath):
+                    pileupText = self.decodeArray(arrayPath)
+
+                    print(arrayPath.split('/')[-1])
+                    print(label)
+                    for r, row in enumerate(pileupText):
+                        print(row)
+                else:
+                    print("WARNING file not found: ",arrayPath)
+                    pass
+
+
+    def convertArraysToPNG(self):
+        with open(self.summaryFile,'r') as file:
+            for line in file:
+                arrayPath, label = line.split(',')
+
+                if self.localPath is not None:
+                    arrayFilename = arrayPath.split('/')[-1]
+                    arrayPath = self.localPath + arrayFilename
+
+                if os.path.exists(arrayPath):
+                    self.convertArrayToRGBA(arrayPath)
+                else:
+                    print("WARNING file not found: ",arrayPath)
+                    pass
+
+
+    def decodeRGBstoStdout(self):
+        with open(self.summaryFile,'r') as file:
+            for line in file:
+                imagePath, label = line.split(',')
+
+                if self.localPath is not None:
+                    imageFilename = imagePath.split('/')[-1]
+                    imagePath = self.localPath + imageFilename
+
+                if os.path.exists(imagePath):
+                    pileupText = self.decodeRGB(imagePath)
+
+                    print(imagePath.split('/')[-1])
+                    for r, row in enumerate(pileupText):
+                        print(label[r], row)
+                else:
+                    print("WARNING file not found: ",imagePath)
+                    pass
+
+
+    def RGBtoBinary(self,rgb):
+        return [int(value/255) for value in rgb]
+
+
+    def RGBtoSortingKey(self,rgb):
+        i1,i2,i3 = self.RGBtoBinary(rgb)
+        code = self.RGBtoText[i1][i2][i3]
+        return self.sortingKey[code]
+
+
+    def convertArrayToRGBA(self,npyFilePath):
+        array = numpy.load(npyFilePath)
+
+        width,height,depth = array.shape
+
+        image = Image.new("RGBA", (height, width))
+        pixels = image.load()
+
+
+        for w in range(width):
+            for h in range(height):
+                channels = array[w,h,:-1]
+                quality = array[w,h,depth-1]     # q is always last channel
+                decodeIndex = numpy.sum(channels*self.decodeIndex)
+                snp = self.decodeToSNPMap[decodeIndex]
+
+                print(snp)
+                rgb = self.SNPtoRGB[snp]
+                # print(rgb)
+                # print(quality)
+                rgba = rgb+[quality*255]
+                # print(rgba)
+                rgba = tuple(rgba)
+                pixels[h,w] = rgba
+
+        pngFilePath = npyFilePath.split('.')[0] + ".png"
+        image.save(pngFilePath, "PNG")
+
+
+    def decodeArray(self,npyFilePath):
+        array = numpy.load(npyFilePath)
+
+        width,height,depth = array.shape
+
+        text = list()
+
+        for w in range(width):
+            row = list()
+            for h in range(height):
+                channels = array[w,h,:-1]
+                index = numpy.sum(channels*self.decodeIndex)
+                row.append(self.decodeToTextMap[index])
+
+            text.append(''.join(row))
+
+        return text
+
+
+    def decodeRGB(self,imageFilePath):
+        '''
+        Read a RGB and convert to a text alignment
+        :param filename:
+        :return:
+        '''
+
+        image = Image.open(imageFilePath)
+
+        pixels = numpy.array(image.getdata())
+        text = list()
+
+        width,height = image.size
+        depth = 4
+
+        pixels = numpy.reshape(pixels,(height,width,depth))
+
+        for h in range(height):
+            row = list()
+            for w in range(width):
+                r,g,b,a = self.RGBtoBinary(pixels[h][w])
+                row.append(self.RGBtoText[r][g][b])
+
+            text.append(''.join(row))
+
+        return text
+
+
+# decoder = Decoder("/Users/saureous/code/Git/pileupGenerator/data/nChannels/summary_3:100000-200000.csv",
+#                   localPath="/Users/saureous/code/Git/pileupGenerator/data/nChannels/")
+#
+# decoder.convertArraysToPNG()
