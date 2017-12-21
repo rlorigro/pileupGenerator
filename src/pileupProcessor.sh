2a64e28af377506e7e3d9abe141b664fd938c0d4
Kishwar <kishwar.shafin@gmail.com>
2017-12-20 21:33:13 -0500

    bash script updated

--- src/pileupProcessor.sh
+++ src/pileupProcessor.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if [ $# -ne 7 ];
+if [ $# -ne 8 ];
 then
-echo "usage: "$(basename $0) "[bam-file]" "[ref-file]" "[vcf-file]" "[coverage]" "[window-size]" "[output_dir]" "[max_threads]"
+echo "usage: "$(basename $0) "[bam-file]" "[ref-file]" "[vcf-file]" "[confident-vcf]" "[coverage]" "[window-size]" "[output_dir]" "[max_threads]"
 exit
 fi
 
 bam_file=$1
 ref_file=$2
 vcf_file=$3
-coverage=$4
-window_size=$5
-output_dir=$6
-max_threads=$7
+confident_vcf_file=$4
+coverage=$5
+window_size=$6
+output_dir=$7
+max_threads=$8
 now=$(date '+%d%m%Y%H%M%S')
 output_dir=$output_dir'run-'$now
 mkdir $output_dir
@@ -24,5 +25,5 @@ for i in ${chrs[@]};
     current_output_dir=$output_dir/'chr'$i/
     mkdir $current_output_dir
     echo "Starting chr" $i
-    python3 pileupGenerator.py --bam $bam_file --ref $ref_file --vcf $vcf_file --vcf_region $i --coverage $coverage --window_size $window_size --output_dir $current_output_dir --max_threads $max_threads 2>tmp/progress-$i.txt &
+    python3 pileupGenerator.py --bam $bam_file --ref $ref_file --vcf $vcf_file  --vcf_confident $confident_vcf_file --vcf_region $i --coverage $coverage --window_size $window_size --output_dir $current_output_dir --max_threads $max_threads 2>tmp/progress-$i.txt &
     done
